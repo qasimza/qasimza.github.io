0763c6dc7e137a95a316583f7e6bb2502aa51099
Zaina Qasim <qasimza@mail.uc.edu>
2024-03-15 23:52:50 -0400

    Updated navbar

--- professional-portfolio/src/Sections/Navbar.tsx
+++ professional-portfolio/src/Sections/Navbar.tsx
@@ -20,17 +20,18 @@ const Navbar = () => {
     const [mouseIn, setMouseIn] = useState('');
 
     return (
-        <header id="header" className="flex flex-col justify-center">
-            <nav className="m-0 p-0">
+        <header id="header" className="fixed flex flex-col justify-center top-0 left-0 bottom-0 z-[9997] p-5 overflow-y-auto">
+            <div className="m-0 p-0">
                 <ul className="list-none">
                     {
                         navbarItems.map((item: NavbarItem) => (
-                            <li key={item.name} className={`${item.className}`}>
+                            <li key={item.name} className={`${item.className}`}
+                            onMouseOver={() => setMouseIn(item.name)}
+                            onMouseOut={() => setMouseIn('')}
+                            >
                                 <a
                                     href={`#${item.name}`}
-                                    className="flex items-center text-body-2 px-4 mb-2 rounded-full bg-body-3 h-14 w-100 overflow-hidden hover:text-body-3 hover:bg-blue"
-                                    onMouseOver={() => setMouseIn(item.name)}
-                                    onMouseOut={() => setMouseIn('')}
+                                    className="flex items-center text-body-2 px-4 mb-2 rounded-full bg-body-3 h-14 w-100 overflow-hidden hover:text-body-3 hover:bg-blue transition-all"
                                 >
                                     <span className="material-symbols-outlined text-xl">{item.icon}</span>
                                     <span className={`pl-3 ${mouseIn === item.name ? '' : 'hidden'}`}>{item.label}</span>
@@ -39,7 +40,7 @@ const Navbar = () => {
                         ))
                     }
                 </ul>
-            </nav>
+            </div>
         </header>
     );
 };
