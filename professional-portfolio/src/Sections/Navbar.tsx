2a610c4e11d0760afdb528927351657c56f116da
Zaina Qasim <qasimza@mail.uc.edu>
2024-03-16 06:28:18 -0400

    Checkpoint

--- professional-portfolio/src/Sections/Navbar.tsx
+++ professional-portfolio/src/Sections/Navbar.tsx
@@ -22,25 +22,22 @@ const Navbar = () => {
     return (
         <header id="header" className="fixed flex flex-col justify-center top-0 left-0 bottom-0 z-[9997] p-5 overflow-y-auto">
             <div className="m-0 p-0">
-                <ul className="list-none">
                     {
-                        navbarItems.map((item: NavbarItem) => (
-                            <li key={item.name} className={`${item.className}`}
-                            onMouseOver={() => setMouseIn(item.name)}
-                            onMouseOut={() => setMouseIn('')}
+                    navbarItems.map((item: NavbarItem) => (
+                            
+                        <a
+                            href={`#${item.name}`}
+                            className="flex items-center text-body-2 px-4 mb-2 rounded-full bg-body-3 h-14 overflow-hidden hover:text-body-3 hover:bg-blue"
+                            onMouseEnter={() => setMouseIn(item.name)}
+                            onMouseLeave={() => setMouseIn('')}
                             >
-                                <a
-                                    href={`#${item.name}`}
-                                    className="flex items-center text-body-2 px-4 mb-2 rounded-full bg-body-3 h-14 w-100 overflow-hidden hover:text-body-3 hover:bg-blue transition-all"
-                                >
                                     <span className="material-symbols-outlined text-xl">{item.icon}</span>
                                     <span className={`pl-3 ${mouseIn === item.name ? '' : 'hidden'}`}>{item.label}</span>
                                 </a>
-                            </li>
+                            
                         ))
                     }
-                </ul>
-            </div>
+           </div>
         </header>
     );
 };
